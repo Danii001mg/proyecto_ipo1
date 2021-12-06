b2fbcf21c86fc5ac5fe6772c619918e5e3886db5
danii <danii@DESKTOP-6M8BSPL>
2021-12-06 12:51:35 +0100

    Agregar archivos de proyecto.

---
+++ proyecto_ipo1/App.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace proyecto_ipo1
+{
+    /// <summary>
+    /// Lógica de interacción para App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static void SelectCulture(string culture)
+        {
+            if (String.IsNullOrEmpty(culture))
+                return;
+            // Copiamos todos los diccionarios de recursos en una lista auxiliar
+            var dictionaryList = Application.Current.Resources.MergedDictionaries.ToList();
+            // Buscamos el idioma (culture) especifico entre ellos
+            string requestedCulture = string.Format("DiccionarioRecursos.{0}.xaml", culture);
+            var resourceDictionary = dictionaryList.
+            FirstOrDefault(d => d.Source.Equals("recursos/" + requestedCulture));
+            if (resourceDictionary == null)
+            {
+                // Si no lo encontramos, aplicamos el idioma por defecto
+                requestedCulture = "DiccionarioRecursos.xaml";
+                resourceDictionary = dictionaryList.
+                FirstOrDefault(d => d.Source.OriginalString == requestedCulture);
+            }
+            // Si lo encontramos, lo borramos de la lista y lo posicionamos el ultimo
+            // Utilizamos las cadenas de este diccionario
+            if (resourceDictionary != null)
+            {
+                Application.Current.Resources.MergedDictionaries.Remove(resourceDictionary);
+                Application.Current.Resources.MergedDictionaries.Add(resourceDictionary);
+            }
+            // Informamos del idioma que estamos utilizando actualmente
+            Thread.CurrentThread.CurrentCulture = new CultureInfo(culture);
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo(culture);
+        }
+    }
+}
+
---
+++ proyecto_ipo1/MainWindow.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace proyecto_ipo1
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int contador = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+            App.SelectCulture("es-ES");      
+        }
+
+        private void comprobarUsuario(object sender, RoutedEventArgs e)
+        {
+            string usuario = "admin";
+            VentanaUsuario ventanaNueva = new VentanaUsuario();
+            if (!String.IsNullOrEmpty(tbxEmail.Text) && tbxEmail.Text.Equals(usuario, StringComparison.InvariantCultureIgnoreCase))
+            {
+                // comprobación correcta
+                pbxContraseña.IsEnabled = true;
+                tbxEmail.BorderBrush = Brushes.Transparent;
+                comprobarContraseña(pbxContraseña.Password);
+                if (contador++ == 2)
+                {
+                    Close();
+                    ventanaNueva.Show();
+                }
+
+            }
+            else
+            {
+                // comprobación errónea
+                tbxEmail.BorderBrush = Brushes.Red;
+                tbxEmail.BorderThickness = new Thickness(2);
+                if (pbxContraseña.IsEnabled)
+                {
+                    pbxContraseña.IsEnabled = false;
+                }
+                contador = 0;
+            }
+        }
+
+        private void comprobarContraseña(string contraseña)
+        {
+            string pass_usuario = "admin";
+            if (pbxContraseña.IsEnabled && !String.IsNullOrEmpty(pbxContraseña.Password))
+            {
+                if (!pbxContraseña.Password.Equals(pass_usuario))
+                {
+                    // marcamos borde en rojo
+                    pbxContraseña.BorderBrush = Brushes.Red;
+                    pbxContraseña.BorderThickness = new Thickness(2);
+                }
+                else
+                {
+                    // restauramos estado normal del borde
+                    pbxContraseña.BorderBrush = Brushes.Transparent;
+                }
+            }
+        }
+
+        private void salir(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void cb_elementoSeleccionado(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBoxItem cbi = (ComboBoxItem)cbIdiomas.SelectedItem;
+            string selectedText = cbi.Content.ToString();
+            if ((selectedText.Equals("ES") || selectedText.Equals("SP"))
+            && !CultureInfo.CurrentCulture.Name.Equals("es-ES"))
+            {
+                App.SelectCulture("es-ES");
+            }
+            else if (selectedText.Equals("EN")
+            && !CultureInfo.CurrentCulture.Name.Equals("en-US"))
+            {
+                App.SelectCulture("en-US");
+            }
+        }
+    }
+
+   
+}
